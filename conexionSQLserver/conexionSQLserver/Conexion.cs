9793938e4cf9a429dd564b3e93cdcfbbc886c9cf
Oscar Bautista <oscar04262000@gmail.com>
2020-08-19 00:35:43 -0500

    Enpezamos un nuevo proyecto de conexion SQLServer:

---
+++ conexionSQLserver/conexionSQLserver/Conexion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace conexionSQLserver
+{
+    class Conexion
+    {
+        public static SqlConnection Conectar()
+        {
+            SqlConnection nc = new SqlConnection("SERVER=LAPTOP-2CE4BE9L;DATABASE=visualStudio;Integrated security=true");
+            nc.Open();
+            return nc;
+        }
+    }
+}
---
+++ conexionSQLserver/conexionSQLserver/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace conexionSQLserver
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+            Conexion.Conectar();
+            
+        }
+    }
+}
